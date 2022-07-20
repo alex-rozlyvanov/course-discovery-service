fabf1cd97510943a2d609204f336f5ac6bd35d8b
oleksandr.rozlyvanov@sombrainc.com <oleksandr.rozlyvanov@sombrainc.com>
2022-07-20 19:27:28 +0300

    Fixed stop_application.sh

--- stop_application.sh
+++ stop_application.sh
@@ -3,7 +3,6 @@ docker stop discovery && docker rm discovery && docker rmi discovery
 docker stop api-gateway && docker rm api-gateway && docker rmi api-gateway
 
 cd ../course-authenticator && docker-compose down && docker-compose rm -f
-cd ../course-manager && docker-compose down && docker-compose rm -f \
-&& docker-compose -f docker-compose-kafka-simple.yml down && docker-compose -f docker-compose-kafka-simple.yml rm -f
+cd ../course-manager && docker-compose down --remove-orphan && docker-compose rm -f
 cd ../course-event-processor && docker-compose down && docker-compose rm -f
 cd ../course-journal && docker-compose down && docker-compose rm -f
