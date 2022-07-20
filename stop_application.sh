a455d6e52366d7a90bff559706857a4b8ba5c08c
oleksandr.rozlyvanov@sombrainc.com <oleksandr.rozlyvanov@sombrainc.com>
2022-07-20 17:15:29 +0300

    Updated scripts

--- stop_application.sh
+++ stop_application.sh
@@ -1,8 +1,9 @@
 
- # start discovery
 docker stop discovery && docker rm discovery && docker rmi discovery
 docker stop api-gateway && docker rm api-gateway && docker rmi api-gateway
 
-cd ../course-authenticator && docker-compose down && docker-compose rm
-cd ../course-manager && docker-compose down && docker-compose rm
-cd ../course-journal && docker-compose down && docker-compose rm
+cd ../course-authenticator && docker-compose down && docker-compose rm -f
+cd ../course-manager && docker-compose down && docker-compose rm -f \
+&& docker-compose -f docker-compose-kafka-simple.yml down && docker-compose -f docker-compose-kafka-simple.yml rm -f
+cd ../course-event-processor && docker-compose down && docker-compose rm -f
+cd ../course-journal && docker-compose down && docker-compose rm -f
